2b260498492129f71f513bae785b34fffb46933f
Dario Evers <darioevers@gmail.com>
2021-06-15 23:43:55 +0200

    fixed menu and navigation

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // importing all components
-import Navigation from "./components/Nav";
+import Nav from "./components/Nav";
 import Home from "./components/Home";
 import About from "./components/AboutMe";
 import Projects from "./components/Projects";
@@ -11,15 +11,10 @@ import Contact from "./components/Contact";
 
 
 function App() {
-  console.log(process.env.PUBLIC_URL);
   return (
     <Router>
-      <Navigation />
-
+      <Nav />
       <Switch>
-        {
-          // <Route path={process.env.PUBLIC_URL + "/"} exact component={Home} />
-        }
         <Route path="/" exact component={Home} />
         <Route path="/about" exact component={About} />
         <Route path="/projects" exact component={Projects} />
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ const Home = () => {
   return (
     <div className="home-wrapper">
       <header className="hero">HOME</header>
+      <div>TEST TEST TEST</div>
     </div>
   );
 };
