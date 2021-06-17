44fa5f98869ea5634aa34187298b844b7b2402c8
Dario Evers <darioevers@gmail.com>
2021-06-17 08:38:37 +0200

    updated service frames

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,17 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import serviceimg from './images/service-1.svg';
+import webdevimg from './images/web-dev-1.svg';
+
 const Services = () => {
   return (
     <div class="main">
     <div class="main_content">
-    <div class="about_content">
-      <div class="about_content_top">
+    <div class="services_content">
+      <div class="services_content_top">
         <h1>Services</h1>
       </div>
-      <div class="about_content_mid_l">
+      <div class="services_content_mid_l leftbox">
+      <img src={serviceimg} class="avatar"></img>
+        <h3>UI/UX DESIGN</h3>
+        <p>Lorem3</p>
       </div>
-      <div class="about_content_mid_r">
+      <div class="services_content_mid_m middlebox">
+      <img src={webdevimg} class="avatar"></img>
+        <h3>UI/UX DESIGN</h3>
+        <p>Lorem3</p>
       </div>
-      <div class="about_content bot"></div>
+      <div class="services_content_mid_r rightbox">
+      <img src={webdevimg} class="avatar"></img>
+        <h3>UI/UX DESIGN</h3>
+        <p>Lorem3</p>
+      </div>
+      <div class="services_content_bot">
+      <p>Looking for a custom job? Contact me<Link to="/contact"> here. 👋</Link></p>
+      </div>
     </div>
     </div>
     </div>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,8 +2,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => {
   return (
